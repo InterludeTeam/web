6902fadda87b2de7795198d63493faba32ffebff
Aleksey Lozovagin 1 <Oleksih@gmail.com>
2018-09-26 19:53:44 +0300

    create order

--- src/components/pages/Menu.vue
+++ src/components/pages/Menu.vue
@@ -2,7 +2,7 @@
     <section>
         <div class="columns is-mobile is-multiline">
             <div class="column is-one-third-desktop is-full-mobile is-full-tablet mobile-padding" v-bind:key="item.id"
-                 v-for="item in items">
+                 v-for="(item, index) in items">
                 <div class="card">
                     <div class="card-image">
                         <figure class="image is-1by1">
@@ -13,6 +13,9 @@
                         {{item.title}}
                     </div>
                     <div class="card-content">ТУТ буде склад (короткий) як тiльки зробемо №2</div>
+                    <footer class="card-footer">
+                        <a class="card-footer-item" @click.stop="buyModal(index)">Замовити</a>
+                    </footer>
                 </div>
             </div>
         </div>
@@ -38,6 +41,38 @@
                 this.$http.get('https://apiinterlude.in-story.org/api/method/menu.get').then(response => {
                     if (response.body.success == true)
                         this.items = response.body.response;
+                });
+            },
+            buyModal(index){
+                var item = this.items[index];
+                this.$dialog.prompt({
+                    title: 'Замовити '+item.title,
+                    message: 'Скiльки?',
+                    confirmText: "Замовити",
+                    inputAttrs: {
+                        type: 'number',
+                        // placeholder: '',
+                        value: '1',
+                        maxlength: 4,
+                        min: 1,
+                    },
+                    onConfirm: (amount) => this.buy(item.id, amount)
+                })
+            },
+            buy(item_id, amount){
+                this.$http.get('https://apiinterlude.in-story.org/api/method/bill.pay', {
+                    body: {
+                        item_id: item_id,
+                        amount: amount
+                    }
+                }).then(response => {
+                    if (response.body.success == true){
+                        this.$toast.open({
+                            message: 'Замовленя додано в обробку',
+                            type: 'is-success'
+                        })
+                    }
+
                 });
             }
         }
